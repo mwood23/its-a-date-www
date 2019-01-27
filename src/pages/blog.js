912784c9cf5241f53580bc04653f1066e008e30f
Marcus Wood <marcuswood230@gmail.com>
2019-01-27 15:01:22 -0500

    Fix up recent blog posts page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,15 +3,46 @@ import Layout from '../components/layout';
 import { PageWrapper, Container } from '../components/common';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import { media } from '../utils/theme';
 
 const BlogItem = styled.div`
-  box-shadow: 0 1px 8px 0 rgba(186, 186, 186, 0.5);
   margin-bottom: 1rem;
   padding: 1.5rem;
   text-decoration: none !important;
   color: ${props => props.theme.defaultFontColor} !important;
   border-radius: 5px;
   transition: 100ms linear;
+`;
+
+const BlogItemContainer = styled.div`
+  display: flex;
+  box-shadow: 0 1px 8px 0 rgba(186, 186, 186, 0.5);
+
+  ${media.forSmallOnly`
+    flex-direction: column;
+  `};
+
+  img {
+    width: 300px;
+    height: 300px;
+    object-fit: cover;
+    margin: 0;
+
+    ${media.forSmallMediumOnly`
+      display: none;
+      height: 200px;
+    `};
+
+    ${media.forSmallOnly`
+      display: block;
+      width: 100%;
+    `};
+  }
+
+  > div {
+    flex: 1;
+    margin: 0;
+  }
 
   &:hover {
     box-shadow: 0 3px 14px 0 rgba(186, 186, 186, 0.5);
@@ -21,15 +52,15 @@ const BlogItem = styled.div`
 const DateAuthorLine = styled.p`
   color: #959595;
   font-size: 0.9rem;
+  margin-bottom: 1rem;
 `;
 
 const Blog = ({ data }) => {
   const { edges: posts } = data.allMarkdownRemark;
 
-  console.log(posts);
   return (
     <Layout>
-      <PageWrapper style={{ marginTop: '3rem' }}>
+      <PageWrapper style={{ marginTop: '3rem', paddingBottom: '3rem' }}>
         <Container>
           <h1>Blog</h1>
           <p>
@@ -42,16 +73,26 @@ const Blog = ({ data }) => {
                 to={post.node.fields.slug}
                 style={{ textDecoration: 'none' }}
               >
-                <BlogItem key={post.node.id}>
-                  <h3 style={{ marginBottom: '5px' }}>
-                    {post.node.frontmatter.title}
-                  </h3>
-                  <DateAuthorLine>
-                    {post.node.frontmatter.date} /{' '}
-                    {post.node.frontmatter.author.id}
-                  </DateAuthorLine>
-                  <p>{post.node.excerpt}</p>
-                </BlogItem>
+                <BlogItemContainer>
+                  <img
+                    src={post.node.frontmatter.heroImage}
+                    alt={
+                      post.node.frontmatter.heroImageAlt
+                        ? post.node.frontmatter.heroImageAlt
+                        : 'hero image for blog post'
+                    }
+                  />
+                  <BlogItem key={post.node.id}>
+                    <h3 style={{ marginBottom: '5px' }}>
+                      {post.node.frontmatter.title}
+                    </h3>
+                    <DateAuthorLine>
+                      {post.node.frontmatter.date} /{' '}
+                      {post.node.frontmatter.author.id}
+                    </DateAuthorLine>
+                    <p>{post.node.excerpt}</p>
+                  </BlogItem>
+                </BlogItemContainer>
               </Link>
             );
           })}
@@ -69,7 +110,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt(pruneLength: 400)
+          excerpt(pruneLength: 300)
           id
           fields {
             slug
@@ -77,6 +118,8 @@ export const pageQuery = graphql`
           frontmatter {
             title
             templateKey
+            heroImage
+            heroImageAlt
             date(formatString: "MMMM DD, YYYY")
             author {
               id
