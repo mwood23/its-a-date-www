e4b01d1e648d4b15aad608c1672fd1b3c3f0aa67
Marcus Wood <marcuswood230@gmail.com>
2019-01-27 13:43:59 -0500

    Add Cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,9 @@
 import CMS from 'netlify-cms';
 import ResponsiveVimeoEditorWidget from 'netlify-cms-responsive-vimeo-editor-widget';
+import ResponsiveYouTubeEditorWidget from 'netlify-cms-responsive-youtube-editor-widget';
 
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 
 CMS.registerEditorComponent(ResponsiveVimeoEditorWidget);
+CMS.registerEditorComponent(ResponsiveYouTubeEditorWidget);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
